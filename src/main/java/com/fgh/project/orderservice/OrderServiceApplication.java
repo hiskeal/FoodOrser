b0e32fabccc2ab053d904df6bdea0e73238267c7
Hiskeal GitHub <hisfincho@gmail.com>
2021-11-29 17:01:22 -0500

    my changes

--- src/main/java/com/fgh/project/orderservice/OrderServiceApplication.java
+++ src/main/java/com/fgh/project/orderservice/OrderServiceApplication.java
@@ -1,12 +1,17 @@
 package com.fgh.project.orderservice;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+import com.sun.org.apache.xerces.internal.impl.xs.models.XSCMUniOp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello There!!");
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
